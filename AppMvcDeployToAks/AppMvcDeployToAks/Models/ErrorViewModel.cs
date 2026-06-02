c32ab17e788e65cecf82879d9c912de77bfcf82c
jbuendia_oxxogh <jbuendia@bside.com.mx>
2026-06-01 19:30:57 -0600

    Add web app mvc and file workflows

---
+++ AppMvcDeployToAks/AppMvcDeployToAks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppMvcDeployToAks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
